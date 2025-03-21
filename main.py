87cb879ecbe3fa6c3fe94785de7df14e609987b9
Abel Gebreyesus <abelgebreyesus2@gmail.com>
2025-03-20 20:06:34 -0400

    Add main

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from constants import *
 
 def main():
   print('Starting Asteroids!')
-  print('Screen width: ' + str(SCREEN_WIDTH))
-  print('Screen height: ' + str(SCREEN_HEIGHT))
+  
+  dt = 0.0
+  clock = pygame.time.Clock()
+
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
     screen.fill(BLACK)
+    dt = clock.tick(60) / 1000
     pygame.display.flip()
-
+    
 if __name__ == '__main__':
   main()
